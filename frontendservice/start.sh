29a2206d7feebecfda94304688623ecb1d9d74a3
Uchiman <ismailcelebi7@gmail.com>
2024-09-18 16:31:27 +0300

    added ssl and edited nginx conf, everything working well just need signed ssl settings

---
+++ frontendservice/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p /etc/nginx/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/CN=localhost"
+
+nginx -g "daemon off;"
