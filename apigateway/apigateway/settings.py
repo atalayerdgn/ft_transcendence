2cc34c05585569b85c820d1fa3cf15653fd75dba
Eozdur <emreozdur06@gmail.com>
2024-11-26 02:19:43 +0300

    Intra redirect page added and profile page updated.Env variables have been updated and all variables moved to one page

--- apigateway/apigateway/settings.py
+++ apigateway/apigateway/settings.py
@@ -15,6 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('API_SECRET_KEY')
 
+USER_SECRET_KEY = os.getenv('USER_MANAGEMENT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
